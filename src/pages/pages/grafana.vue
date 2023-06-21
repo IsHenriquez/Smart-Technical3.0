a908aba0a34b82e0fee4c4cea3866e48480338fc
IsHenriquez <is.henriquez@duocuc.cl>
2023-06-21 07:32:05 -0400

    grafana

--- src/pages/pages/grafana.vue
+++ src/pages/pages/grafana.vue
@@ -1,5 +1,30 @@
+<script setup>
+import panelesGrafana from "@/views/apps/panelesGrafana.vue"
+
+
+</script>
+
+
 <template>
-  <VLayout>
-    <VMain />
-  </VLayout>
+  <v-container fluid>
+  <v-layout>
+<h2>
+  Paneles configurados actualmente
+</h2>
+
+    <panelesGrafana />
+
+    <v-main/>
+  </v-layout>
+</v-container>
 </template>
+
+<style>
+h2 {
+  writing-mode: vertical-lr;
+  text-align: center;
+}
+.down {
+  padding-top: 10px;
+}
+</style>

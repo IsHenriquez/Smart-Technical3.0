78e2055f7534903663b9655934fc8c683a01a629
IsHenriquez <is.henriquez@duocuc.cl>
2023-06-22 22:14:54 -0400

    changes in map

--- src/pages/apps/Mapas.vue
+++ src/pages/apps/Mapas.vue
@@ -1,67 +1,72 @@
 <template>
-  <div class="map-wrap">
-    <a
-      href="https://www.maptiler.com"
-      class="watermark"
-    />
-    <div
-      ref="mapContainer"
-      class="map"
-    />
+  <div class="map-container">
+    <div class="map-wrapper">
+      <GoogleMap
+        api-key="AIzaSyBBI-D26YoQUTnXBNGrqQU0pAagnh0V1hw"
+        style="width: 100%; height: 500px"
+        :center="center"
+        :zoom="15"
+      >
+        <Marker :options="markerOptions" />
+        <Marker :options="markerOptionss" />
+      </GoogleMap>
+    </div>
+    <table>
+      <tr>
+        <th>Columna 1</th>
+        <th>Columna 2</th>
+        
+      </tr>
+      <tr>
+        <td>Dato 1</td>
+        <td>Dato 2</td>
+
+      </tr>
+      <tr>
+        <td>Dato 4</td>
+        <td>Dato 5</td>
+
+      </tr>
+    </table>
   </div>
 </template>
 
 <script>
-import { Map } from 'maplibre-gl'
-import { markRaw, onMounted, onUnmounted, shallowRef } from 'vue'
+import { defineComponent } from "vue";
+import { GoogleMap, Marker } from "vue3-google-map";
 
-export default {
-  name: "Mapa",
-  setup () {
-    const mapContainer = shallowRef(null)
-    const map = shallowRef(null)
-    
+export default defineComponent({
+  components: { GoogleMap, Marker },
+  setup() {
+    const center = { lat: -33.45694, lng: -70.64827 };
+    const markerOptions = {
+      position: { lat: -33.45555287904146, lng: -70.64485942725804 },
+      label: "s",
+      title: "LADY LIBERTY",
+    };
+    const markerOptionss = {
+      position: { lat: -33.449394224322575, lng: -70.65146839016886 },
+      label: "t",
+      title: "LADY LIBERTY",
+    };
 
-    onMounted(() => {
-      const apiKey = 'NHyhDHUf15vaw2foSq6C'
-
-      const initialState = { lng: -70.64827, lat: -33.45694, zoom: 12 }
-
-      
-
-      map.value = markRaw(new Map({
-        container: mapContainer.value,
-        style: `https://api.maptiler.com/maps/streets-v2/style.json?key=${apiKey}`,
-        center: [initialState.lng, initialState.lat],
-        zoom: initialState.zoom,
-
-      }))
-      
-    }),
-    onUnmounted(() => {
-      map.value?.remove()
-    })
-
-    return {
-      map, mapContainer,
-    }
+    return { center, markerOptions, markerOptionss };
   },
-}
+});
 </script>
 
-
-<style scoped>
-@import "node_modules/maplibre-gl/dist/maplibre-gl.css";
-
-.map-wrap {
-  position: relative;
-  block-size: calc(100vh - 77px); /* calculate height of the screen minus the heading */
-  inline-size: 100%;
+<style>
+.map-container {
+  display: flex;
 }
 
-.map {
-  position: absolute;
-  block-size: 90%;
-  inline-size: 90%;
+.map-wrapper {
+  width: 80%;
+  height: 500px;
+}
+
+table {
+  width: 20%;
+  margin-left: 20px;
 }
 </style>

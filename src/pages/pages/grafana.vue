3675fcbf08855ae6a3372ed5cbdf6011cf665667
benja2203 <barayafernandez@gmail.com>
2023-06-28 21:01:02 -0400

    request delete ticket

--- src/pages/pages/grafana.vue
+++ src/pages/pages/grafana.vue
@@ -2,7 +2,8 @@
   <div class="iframe-container">
     <Vcard class="cardPanel">
       <h1 style="text-align: center;">Monitoreo en tiempo real Grafana</h1>
-      <iframe class="responsive-iframe" src="http://35.203.149.123:3000/d/sF7d-FHZz/smartki-monitoring?orgId=1&from=1687884934301&to=1687886734301"></iframe>
+      <iframe class="responsive-iframe"
+        src="http://35.203.149.123:3000/d/sF7d-FHZz/smartki-monitoring?orgId=1&from=1687884934301&to=1687886734301"></iframe>
     </Vcard>
   </div>
   <br>
@@ -11,16 +12,16 @@
 <style>
 .iframe-container {
   position: relative;
-  width: 100%;
-  padding-bottom: 56.25%; /* Aspect ratio 16:9 */
   overflow: hidden;
+  inline-size: 100%;
+  padding-block-end: 56.25%;
 }
 
 .responsive-iframe {
   position: absolute;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
+  block-size: 100%;
+  inline-size: 100%;
+  inset-block-start: 0;
+  inset-inline-start: 0;
 }
 </style>
